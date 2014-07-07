0da6e5e2ff3842635db70e89fa5c39a147a5d029
Jakub Kuchar <jakub.kuchar@hotmail.com>
2014-07-07 13:38:43 +0700

    better nothing found

--- lib/rapportive/rapportive_api.rb
+++ lib/rapportive/rapportive_api.rb
@@ -35,7 +35,7 @@ module Rapportive
       attempts = options[:attempts]
       response = execute_query(first_name, last_name, middle_name, domain, proxy_addr, proxy_port)
 
-      return response if response == "Nothing found"
+      return response if response.is_a?(String)
 
       if options[:full_body]
         return response.parsed_response 
